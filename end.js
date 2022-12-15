7f0de036dc12a0a611ea2e8067b67ddf409998e7
LouisOkwaro <okwarolouis@gmail.comgit config --global user.email okwarolouis@gmail.com>
2022-12-15 10:39:49 +0300

    Added event listener to save button

---
+++ end.js
@@ -0,0 +1,5 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
