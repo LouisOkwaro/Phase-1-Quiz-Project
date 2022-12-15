f9f7eabe0032c46f83c56b0dfd59ba999a961e5d
LouisOkwaro <okwarolouis@gmail.comgit config --global user.email okwarolouis@gmail.com>
2022-12-15 10:52:09 +0300

    Made changes to end.js

--- end.js
+++ end.js
@@ -1,10 +1,10 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-finalScore.innerText = mostRecentScore;
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value;
-});
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
 const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
